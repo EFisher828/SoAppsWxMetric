adcfa97a620909e482390ee1f9db8b8945e1206d
EFisher828 <71984782+EFisher828@users.noreply.github.com>
2020-12-14 02:26:21 -0500

    New Wind Barb Plot

--- temp_plot.py
+++ temp_plot.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import shapefile
 import urllib.request
 import matplotlib.pyplot as plt
@@ -335,6 +336,73 @@ def main():
     #plt.show()
 
 main()
+
+def windbarb():
+
+    url = 'https://climate.ncsu.edu/cronos/?station=MITC'
+    html = urllib.request.urlopen(url)
+    soup = BeautifulSoup(html,features='html.parser')
+
+    try:
+            
+        Table = str(soup.find('table', {"class":"CurrentConditions"}).find_all('tr'))
+        Tr = Table.split(',')
+
+        Wind_1 = str(Tr[6])
+        Wind_2 = Wind_1.split('<')
+        Wind_3 = str(Wind_2[9])
+        Wind_4 = Wind_3.split('>')
+        Wind_5 = str(Wind_4[1])
+        Wind_Sum = Wind_5[1:]
+        Dir_1 = Wind_5.split('(')
+        Dir_2 = str(Dir_1[1])
+        Dir_3 = Dir_2.split('°')
+        Dir_4 = eval(Dir_3[0])
+        print(Dir_4)
+        Speed_1 = Wind_5.split('at ')
+        Speed_2 = str(Speed_1[1])
+        Speed_3 = Speed_2.split(' mph')
+        Speed_4 = eval(Speed_3[0])
+
+        Time_1 = str(Tr[3])
+        Time_2 = Time_1.split('<')
+        Time_3 = str(Time_2[0])
+        Time_4 = Time_3.split('@ ')
+        Time_5 = str(Time_4[1])
+        Time_6 = Time_5.split(' (')
+        Time_7 = str(Time_6[0])
+        print(Time_7)
+
+    except:
+        print("Grandfathr missing")
+
+    if Dir_4 < 90:
+        offset = 90 - Dir_4
+        deg_direction = Dir_4 + 90 + 2*offset
+    else:
+        offset = 90 - Dir_4
+        deg_direction = Dir_4 + 90 + 2*offset
+    rad_direction = math.radians(deg_direction)#+(math.pi/2)
+    speed = Speed_4
+    u = speed*math.cos(rad_direction)
+    v = speed*math.sin(rad_direction)
+
+    
+    fig = plt.figure()
+    fig.patch.set_facecolor('grey')
+    ax = plt.axes()
+    ax.set_facecolor('grey')
+    ax.axis('off')
+    ax.barbs(0,0,u,v, length=15, pivot='middle',color='white')
+    plt.xlim(-0.73,0.8)
+    plt.ylim(-0.8,0.8)
+    plt.text(0,-0.6,Wind_Sum,ha='center',size=15,color='white')
+    plt.text(0,-0.75,"Valid: " + Time_7,ha='center',size=15,color='white')
+    plt.text(0,0.75,'Current Winds on Mount Mitchell',color='white',size=20,weight='bold',ha='center')
+
+    plt.savefig("output/barbplot.png",bbox_inches='tight', facecolor=fig.get_facecolor())
+    
+windbarb()
 
 
 
