7c30f1decfd809eaa838be70fa1e45a5f48a7098
EFisher828 <71984782+EFisher828@users.noreply.github.com>
2020-12-16 06:02:50 -0500

    Last second fixes

--- temp_plot.py
+++ temp_plot.py
@@ -295,47 +295,55 @@ def wncbar():
     y_pos = np.arange(len(bars))
 
     #max/mins & fonts
-    maximum = max(height)+3
-    minimum = min(height)-3
-    font = {'size':16,'color':'white'}
-    font2 = {'size':22,'color':'white'}
+    if len(height) > 0:
+        maximum = max(height)+3
+        minimum = min(height)-3
+        font = {'size':16,'color':'white'}
+        font2 = {'size':22,'color':'white'}
 
     #color
-    color_1 = np.array(height)
-    color_2 = cm.cool(1-(color_1 / float(max(color_1))))
-    ax.xaxis.label.set_color('white')
-    ax.spines['bottom'].set_color('white')
-    ax.spines['top'].set_color('white') 
-    ax.spines['right'].set_color('white')
-    ax.spines['left'].set_color('white')
-    ax.tick_params(axis='x', colors='white')
-    ax.tick_params(axis='y', colors='white')
+        color_1 = np.array(height)
+        color_2 = cm.cool(1-(color_1 / float(max(color_1))))
+        ax.xaxis.label.set_color('white')
+        ax.spines['bottom'].set_color('white')
+        ax.spines['top'].set_color('white') 
+        ax.spines['right'].set_color('white')
+        ax.spines['left'].set_color('white')
+        ax.tick_params(axis='x', colors='white')
+        ax.tick_params(axis='y', colors='white')
  
     #horizontal bars
-    plt.barh(y_pos, height, color = color_2)
+        plt.barh(y_pos, height, color = color_2)
  
     #y-axis names
-    plt.yticks(y_pos, bars, **font)
+        plt.yticks(y_pos, bars, **font)
 
     #x-axis
-    plt.xlim(minimum,maximum)
+        plt.xlim(minimum,maximum)
 
     #x label
-    plt.xlabel('Temperature (°F)',**font)
+        plt.xlabel('Temperature (°F)',**font)
 
-    for i, v in enumerate(height):
-        plt.text(v , i-0.1, str(v), color='white', fontsize='13')
-    plt.title('Mountains, NC\nVertical Temperature Profile', **font2)
+        for i, v in enumerate(height):
+            plt.text(v , i-0.1, str(v), color='white', fontsize='13')
+        plt.title('Mountains, NC\nVertical Temperature Profile', **font2)
 
-    now = datetime.now()
-    current_time = now.strftime("%I:%M %p")
-    #plt.text(0.87,0.94,current_time,color='white',size=18,horizontalalignment='center',verticalalignment='center',transform = ax.transAxes)
-    plt.text(-0.27,-0.12,"Source: NCSCO",color='white',size=16,horizontalalignment='center',verticalalignment='center',transform = ax.transAxes)
-    plt.text(-0.27,1.05,"Valid: " + current_time,color='white',size=16,horizontalalignment='center',verticalalignment='center',transform = ax.transAxes)
-    plt.figtext(0.5,-0.2,"AppalachianWX.com",color='white',size=10,horizontalalignment='center',transform = ax.transAxes)
-    #show/save graphic
-    plt.savefig("output/wncbarplot.png",bbox_inches='tight', facecolor=fig.get_facecolor())
-    #plt.show()
+        now = datetime.now()
+        current_time = now.strftime("%I:%M %p")
+        #plt.text(0.87,0.94,current_time,color='white',size=18,horizontalalignment='center',verticalalignment='center',transform = ax.transAxes)
+        plt.text(-0.27,-0.12,"Source: NCSCO",color='white',size=16,horizontalalignment='center',verticalalignment='center',transform = ax.transAxes)
+        plt.text(-0.27,1.05,"Valid: " + current_time,color='white',size=16,horizontalalignment='center',verticalalignment='center',transform = ax.transAxes)
+        plt.figtext(0.5,-0.2,"AppalachianWX.com",color='white',size=10,horizontalalignment='center',transform = ax.transAxes)
+        #show/save graphic
+        plt.savefig("output/wncbarplot.png",bbox_inches='tight', facecolor=fig.get_facecolor())
+        #plt.show()
+    elif len(height) == 0:
+        fig = plt.figure()
+        fig.patch.set_facecolor('grey')
+        ax = plt.axes()
+        ax.set_facecolor('grey')
+        plt.text(0.5,0.5,"All Stations Down",color='white',size=22,horizontalalignment='center',verticalalignment='center',transform = ax.transAxes)
+        plt.savefig("output/wncbarplot.png",bbox_inches='tight', facecolor=fig.get_facecolor())
 
 wncbar()
 
